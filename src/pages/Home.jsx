8c71e7457ccd8b6f667c36b83eab0bc1a1fd217f
Yossi Goldstein <ydg7780@gmail.com>
2026-01-12 16:50:38 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -242,7 +242,7 @@ export default function Home() {
         </div>
 
         {/* Navigation Menu on Image */}
-        <div className="relative z-10 w-full">
+        <div className="relative z-[200] w-full">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16 py-4">
               <div className="w-48"></div>
